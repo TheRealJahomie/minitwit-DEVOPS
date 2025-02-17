e673256c6d443142a0aae08eeeef3dd45fe66f2a
LukasA707 <lukas707@hotmail.com>
2025-02-17 21:18:56 +0100

    Switched to unit testing.
    Switched from trying integration tests to unit test of controllers.
    Now in a working state.

--- minitwit.Api.Test/TestWebApi.cs
+++ minitwit.Api.Test/TestWebApi.cs
@@ -1,33 +1,50 @@
 using System.Net.Http.Headers;
-using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using minitwit.Application.Services;
+using minitwit.Controllers;
+using minitwit.Infrastructure.Data;
 using Xunit;
 
 namespace minitwit.Api.Test;
 
 public class TestWebApi : IClassFixture<WebApplicationFactory<Program>>
 {
-    private readonly WebApplicationFactory<Program> _factory;
+    private readonly DbContextOptions<ApplicationDbContext> builder;
+    private readonly SqliteConnection connection;
+    private readonly ApplicationDbContext context;
+    private readonly FollowController followController;
+    private readonly TwitsController twitsController;
+    private readonly UserController userController;
 
-    public TestWebApi (WebApplicationFactory<Program> factory)
+    public TestWebApi ()
     {
-        _factory = factory;
+        connection = new SqliteConnection("Filename=memory;Mode=memory;");
+        connection.Open();
+
+        builder = new DbContextOptionsBuilder<ApplicationDbContext>()
+        .UseSqlite(connection)
+        .Options;
+
+        context = new ApplicationDbContext(builder);
+        context.Database.EnsureCreatedAsync();
+
+        SeedDatabase();
+
+        followController = new FollowController(new FollowService(context));
+        twitsController = new TwitsController(new TwitsService(context));
+        userController = new UserController(new UserSerivce(context));
+    }
+
+    public void SeedDatabase()
+    {
+
     }
 
     [Fact]
     public async Task Test1()
     {
-        // Arrange
-        var client = _factory.CreateClient();
-        client.DefaultRequestHeaders.ExpectContinue = false; 
-        client.DefaultRequestHeaders.Authorization = 
-        new AuthenticationHeaderValue("Bearer", "your_test_jwt_token");
-        string url = "/api/twit/hejkaj";
-
-        // Act
-        var reponse = await client.GetAsync(url);
-
-        // Assert 
-        reponse.EnsureSuccessStatusCode();
+        
     }
 }
